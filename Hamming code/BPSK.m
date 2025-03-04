2aa0a6d782a88dc616225648b1a372ee1905cd38
hassansaker <hassansaker467@gmail.com>
2025-03-04 23:19:17 +0200

    edit file

--- Hamming code/BPSK.m
+++ Hamming code/BPSK.m
@@ -9,7 +9,7 @@ Len = k * 1000; % Number of data bits per frame
 ber1 = zeros(1, L_SNR); 
 ber2 = zeros(1, L_SNR); 
 
-for ii = 1:length(SNR)
+for ii = 1:L_SNR
     num1 = 0; % Number of errors (coded)
     num2 = 0; % Number of errors (uncoded)
     
@@ -17,36 +17,36 @@ for ii = 1:length(SNR)
     pp = 1;
     
     % Uncoded Transmission
-    while (jj < maxF && num2 < 1000)
+    while (jj < maxF && num1 < 1000)
         
         %-----------------Transmitter---------------------
         data = randi([0 1], 1, Len);
-        t2 = 2 * data - 1; % BPSK modulation
+        t1 = 2 * data - 1; % BPSK modulation
         
         %----------------Channel---------------------
-        r2 = awgn(t2, SNR(ii)+10*log10(2)); % Adding noise
+        r1 = awgn(t1, SNR(ii)+10*log10(2)); % Adding noise
         
         %-----------------Receiver----------------------
-        bb2 = r2 > 0; % Demodulation
-        num2 = num2 + biterr(bb2, data);
+        bb1 = r1 > 0; % Demodulation
+        num1 = num1 + biterr(bb1, data);
         jj = jj + 1;
     end
     
     % Coded Transmission
-    while (pp < maxF && num1 < 1000)
+    while (pp < maxF && num2 < 1000)
         
         %-----------------Transmitter---------------------
         data = randi([0 1], 1, Len);
         encData = encode(data', n, k, 'hamming/binary')';
-        t1 = 2 * encData - 1; % BPSK modulation
+        t2 = 2 * encData - 1; % BPSK modulation
         
         %----------------Channel---------------------
-        r1 = awgn(t1, SNR(ii) +10*log10(2)+ 10*log10(k/n)); % Adding noise
+        r2 = awgn(t2, SNR(ii) +10*log10(2)+ 10*log10(k/n)); % Adding noise
         
         %-----------------Receiver----------------------
-        bb1 = r1 > 0; % Demodulation
-        decData = decode(bb1, n, k, 'hamming/binary')';
-        num1 = num1 + biterr(decData', data);
+        bb2 = r2 > 0; % Demodulation
+        decData = decode(bb2, n, k, 'hamming/binary')';
+        num2 = num2 + biterr(decData', data);
         pp = pp + 1;
         
     end
@@ -63,4 +63,4 @@ grid on;
 xlabel('SNR (dB)');
 ylabel('BER');
 title('BER Performance: Coded vs Uncoded BPSK');
-legend('Coded', 'Uncoded');
+legend('Uncoded','Coded');
