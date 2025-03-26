144e167e838bfb5deeec002ccf6fd26bc51c7ae1
hassansaker <hassansaker467@gmail.com>
2025-03-26 21:13:00 +0200

    edit file

--- Convolution Code/Convolution_code_AWGN_PSK.m
+++ Convolution Code/Convolution_code_AWGN_PSK.m
@@ -1,20 +1,48 @@
 clear; clc;
 
 % Simulation Parameters
-constraintLength = 7;            % Constraint length
-codeGenerator = [133 171];       % Octal generator polynomials 
-tracebackDepth = 32;             % Viterbi traceback depth 
-modOrder = 64;                   % 64-QAM modulation
-numBits = 1000*log2(modOrder);   % Number of input bits
-SNR = 0:0.5:20;                  % Signal-to-noise ratio range (dB)
-EbN0 = SNR + 10*log10(modOrder); % Convert SNR to Eb/N0 for BPSK
-L_SNR = length(SNR);             % Number of SNR points
-maxF = 1e3;                      % Maximum number of frames
-
+constraintLength = 3;                  % Constraint length
+codeGenerator = [7 5];                 % Octal generator polynomials 
+tracebackDepth = 16;                   % Viterbi traceback depth 
+M = 4;                                 % BPSK modulation
+numBits =1000*M;                       % Number of input bits
+EbN0 = 0:0.5:10;                       % Signal-to-noise ratio range (dB)
+SNR= EbN0 + 10*log10(log2(M));         % Convert SNR to Eb/N0 for BPSK
+L_SNR = length(SNR);                   % Number of SNR points
+maxF =1e4;                             % Maximum number of frames
+pskModulator = comm.PSKModulator(M,'BitInput',true,'SymbolMapping','Gray','PhaseOffset',0);
+pskDemodulator = comm.PSKDemodulator(M,'BitOutput',true,'SymbolMapping','Gray','PhaseOffset',0);
 ber1 = zeros(1, L_SNR); % BER for uncoded system
-ber2 = zeros(1, L_SNR); % BER for coded system 
+ber2 =zeros(1, L_SNR); % BER for coded system 
 
-while (jj < maxF && num1 < 1000) 
+for i=1:L_SNR
+jj=1;
+pp=1;
+numErrors_coded=0;
+numErrors_uncoded=0;
+
+    while (pp < maxF && numErrors_uncoded < 1000)
+
+    %-----------------Transmitter---------------------
+    inputBits = randi([0 1], numBits, 1); % Generate random binary data
+    % M-PSK Modulation (with bit input & unit average power)
+    txSig_uncoded =pskModulator(inputBits);
+    % constellation(pskModulator); % see the constellation
+    
+    %----------------Channel--------------------------
+    % Add AWGN noise
+    rxSig_uncoded = awgn(txSig_uncoded,SNR(i),'measured');
+    
+    %-----------------Receiver------------------------
+    % M-PSK Demodulation (output as bits)
+    rxDataHard_uncoded = pskDemodulator(rxSig_uncoded);
+    % Calculate Bit Errors 
+    numErrors_uncoded =numErrors_uncoded+ biterr(inputBits,rxDataHard_uncoded );
+    pp=pp+1;  
+    
+    end
+    
+    while (jj < maxF && numErrors_coded < 1000) 
 
     %-----------------Transmitter---------------------
     inputBits = randi([0 1], numBits, 1); % Generate random binary data 
@@ -22,33 +50,35 @@ while (jj < maxF && num1 < 1000)
     % Convolutional Encoding 
     trellis = poly2trellis(constraintLength, codeGenerator);
     encodedBits = convenc(inputBits, trellis);
-
-    % 64-QAM Modulation (with bit input & unit average power)
-    txSig = qammod(encodedBits, modOrder, 'InputType', 'bit', 'UnitAveragePower', true);
     
+    % M-PSK Modulation (with bit input & unit average power)
+    txSig_coded =pskModulator(encodedBits);
+
     %----------------Channel--------------------------
     % Add AWGN noise
-    rxSig = awgn(txSig, EbN0+10*log10(1/2), 'measured');
-
+    rxSig_coded = awgn(txSig_coded,SNR(i)+10*log10(0.5),'measured');
+    
     %-----------------Receiver------------------------
-    % Hard-Decision QAM Demodulation (output as bits)
-    rxDataHard = qamdemod(rxSig, modOrder, 'OutputType', 'bit', 'UnitAveragePower', true);
-
+    % Hard-Decision M-PSK Demodulation (output as bits)
+    rxDataHard_coded = pskDemodulator(rxSig_coded);
     % Viterbi Decoding (continuous mode, hard-decision)
-    decodedBits = vitdec(rxDataHard, trellis, tracebackDepth, 'cont', 'hard');
-
+    decodedBits = vitdec(rxDataHard_coded, trellis, tracebackDepth, 'cont', 'hard');
     % Calculate Bit Errors (skip last 'tbl' bits due to decoding delay)
-    numErrors = biterr(inputBits(1:end-tracebackDepth), decodedBits(tracebackDepth+1:end));
-    ber = numErrors / jj;
-end
+    numErrors_coded =numErrors_coded+ biterr(inputBits(1:end-tracebackDepth), decodedBits(tracebackDepth+1:end));
+    jj=jj+1;
+    
+    end
+    ber1(i) = numErrors_uncoded / pp / numBits;
+    ber2(i) = numErrors_coded / jj / numBits;
 
+end
 % Plot Results
 figure;
-semilogy(SNR, ber1, 'r-*', 'LineWidth', 1.5, 'MarkerSize', 8); % Uncoded
+semilogy(EbN0, ber1, 'r-*', 'LineWidth', 1.5, 'MarkerSize', 8); % Uncoded
 hold on;
-semilogy(SNR, ber2, 'g-h', 'LineWidth', 1.5, 'MarkerSize', 8); % Coded 
+semilogy(EbN0, ber2, 'g-h', 'LineWidth', 1.5, 'MarkerSize', 8); % Coded 
 grid on;
 xlabel('Eb/N0 (dB)');
 ylabel('BER');
-title('BER Performance: Coded vs Uncoded BPSK in Rayleigh Fading Channel');
-legend('Uncoded', 'Coded without Interleaving', 'Coded with Interleaving');
+title(['BER Performance: Coded vs Uncoded ',num2str(M),'-PSK in AWGN Channel']);
+legend('Uncoded', 'Coded');
